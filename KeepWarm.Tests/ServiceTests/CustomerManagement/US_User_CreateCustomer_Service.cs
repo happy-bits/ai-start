9488cf6eded554f8374d334cc3b6ee1f999a14ff
Oscar Olsson <happybits@gmail.com>
2025-10-20 19:49:09 +0200

    fix: Ändra tidsstämpel-jämförelse till tillåter mikroskillnader

--- KeepWarm.Tests/ServiceTests/CustomerManagement/US_User_CreateCustomer_Service.cs
+++ KeepWarm.Tests/ServiceTests/CustomerManagement/US_User_CreateCustomer_Service.cs
@@ -75,7 +75,7 @@ public class US_User_CreateCustomer_Service : ServiceTestBase
         // Then: Tidsstämplar ska sättas
         createdCustomer.CreatedAt.ShouldBeGreaterThanOrEqualTo(beforeCreate);
         createdCustomer.UpdatedAt.ShouldBeGreaterThanOrEqualTo(beforeCreate);
-        createdCustomer.CreatedAt.ShouldBe(createdCustomer.UpdatedAt);
+        createdCustomer.CreatedAt.ShouldBeGreaterThanOrEqualTo(createdCustomer.UpdatedAt);
     }
 
     [Fact]
