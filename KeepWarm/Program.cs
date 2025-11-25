65ef288876d53a656a09101978836af7e21b2b92
Oscar Olsson <happybits@gmail.com>
2025-11-25 13:38:12 +0100

    feat: Upgrade to .net 10

--- KeepWarm/Program.cs
+++ KeepWarm/Program.cs
@@ -61,6 +61,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    // Enable WebSockets for browser refresh in development
+    app.UseWebSockets();
+}
 
 app.UseHttpsRedirection();
 app.UseRouting();
